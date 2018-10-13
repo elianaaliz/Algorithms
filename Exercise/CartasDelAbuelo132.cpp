96f5db04d2148e4b948931dd99e2462160a17290
Eliana Aliz <elianaaliz16@gmail.com>
2018-10-13 16:07:18 +0100

    Update CartasDelAbuelo132.cpp

--- Exercise/CartasDelAbuelo132.cpp
+++ Exercise/CartasDelAbuelo132.cpp
@@ -16,7 +16,6 @@ bool resuelve() {
 		cin.get(letra);
 
 	}
-	cout << frase << ".....+";
 
 	conjuntos[0] = nConjuntos;
 	for (int i = 1; i < n; i++) {
@@ -28,7 +27,7 @@ bool resuelve() {
 
 	int n_Intervalos, a, b;
 	cin >> n_Intervalos;
-	cout << n_Intervalos << "#";
+
 	if (n_Intervalos == 0) {
 		return false;
 	}
@@ -44,36 +43,6 @@ bool resuelve() {
 			cout << "NO\n";
 		}
 	}
-
-
-	/*lO DE AQUI ABAJO NO SIRVE O NO VALE, ESTA MAL, ES MEJOR PENSAR COMO SEMI CONJUNTOS*/
-
-	//cin.get(frase); //leer el char
-	/*cin >> n_intervalos;
-
-	while (n_intervalos-- ) {
-		int a, b;
-		cin >> a;
-		cin >> b;
-		int i = a, j = b;
-		if (b < a) {
-			j = a;
-			i = b;
-		}
-		int l = i + 1;
-		char igual = frase[i];
-		bool correcto = true;
-		while (l < b + 1 && correcto) {
-			if (igual != frase[l]) {
-				correcto = false;
-			}
-		}
-
-		if (correcto) { cout << "SI" << "\n"; }
-		else {
-			cout << "NO" << "\n";
-		}
-	}*/
 
 	cout << "\n";
 	cin.get(letra);
